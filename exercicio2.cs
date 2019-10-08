44f74b8fce87b22632677f6448d29b89dabaf250
marcelomartins123 <marcelomartins1229@gmail.com>
2019-10-07 22:38:20 -0300

    adicionamento do readkey

---
+++ exercicio2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    { 
+        public static int diferenca(int m, int d)
+        {
+            m = m - d;
+            return m;
+        }
+        static void Main(string[] args)
+        {
+            int n,h;
+            Console.Write("digite um valor: ");
+            n = int.Parse(Console.ReadLine());
+            Console.Write("digite outro valor: ");
+            h = int.Parse(Console.ReadLine());
+            Console.WriteLine("resultado é: "+diferenca(n,h));
+            Console.Readtube();
+        }
+    }
+}
